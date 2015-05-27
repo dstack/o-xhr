a68f651b6c3dca49b6e7aeb51c7f5a1886908459
Justin Molis <justin.molis@pearson.com>
2015-05-27 15:53:58 -0600

    Fix lint errors

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,42 +1,38 @@
 /*global require*/
+'use strict';
+
 var xhr =  require('../../main');
 
-document.addEventListener("DOMContentLoaded", function() {
-	"use strict";
-	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-});
-
 document.querySelector('button.clear').onclick = function () {
-  document.getElementById('results').innerText = '';
+	document.getElementById('results').innerText = '';
 };
 
 var buttons = document.querySelectorAll('button.request');
 
 for(var i=0; i<buttons.length; i++) {
-  var button = buttons[i];
-  button.onclick = function () {
-    makeCall(this.innerText);
-  };
+	buttons[i].addEventListener('click', makeCall(buttons[i].innerText));
 }
 
 function makeCall (method) {
-  xhr({
-    url: document.getElementById('url').value,
-    method: method,
-    data: document.getElementById('data').value,
-    onError: function (res) {
-      formatResponse(res);
-    },
-    onComplete: function (res){
-      formatResponse(res);
-    }
-  });
-};
+	return function () {
+		xhr({
+			url: document.getElementById('url').value,
+			method: method,
+			data: document.getElementById('data').value,
+			onError: function (res) {
+				formatResponse(res);
+			},
+			onComplete: function (res){
+				formatResponse(res);
+			}
+		});
+	};
+}
 
 function formatResponse (res) {
-  var pre = document.createElement('pre');
-  var obj = JSON.parse(res);
-  var json = document.createTextNode(JSON.stringify(obj, null, 2));
-  pre.appendChild(json);
-  document.getElementById('results').appendChild(pre);
-};
+	var pre = document.createElement('pre');
+	var obj = JSON.parse(res);
+	var json = document.createTextNode(JSON.stringify(obj, null, 2));
+	pre.appendChild(json);
+	document.querySelector('.results').appendChild(pre);
+}
--- src/js/xhr.js
+++ src/js/xhr.js
@@ -2,30 +2,30 @@
 
 var noop = function () {};
 module.exports = function (settings) {
-  var successCodes = [ 200, 201, 202, 204 ];
+	var successCodes = [ 200, 201, 202, 204 ];
 
-  /* setup defaults */
-  settings.url = settings.url || "";
-  settings.method = settings.method || "GET";
-  settings.onComplete = settings.onComplete || noop;
-  settings.onError = settings.onError || noop;
-  settings.data = settings.data || '';
+	/* setup defaults */
+	settings.url = settings.url || "";
+	settings.method = settings.method || "GET";
+	settings.onComplete = settings.onComplete || noop;
+	settings.onError = settings.onError || noop;
+	settings.data = settings.data || '';
 
-  var r = new XMLHttpRequest();
-  r.open(settings.method, settings.url, true);
-  r.onreadystatechange = function () {
-    if (r.readyState != 4) {
-      return;
-    }
-    else{
-      if (successCodes.indexOf(r.status) === -1) {
-        settings.onError(r.responseText);
-      }
-      else {
-        settings.onComplete(r.responseText);
-      }
-    }
-  };
-  r.send(settings.data);
-  return r;
+	var r = new XMLHttpRequest();
+	r.open(settings.method, settings.url, true);
+	r.onreadystatechange = function () {
+		if (r.readyState !== 4) {
+			return;
+		}
+		else{
+			if (successCodes.indexOf(r.status) === -1) {
+				settings.onError(r.responseText);
+			}
+			else {
+				settings.onComplete(r.responseText);
+			}
+		}
+	};
+	r.send(settings.data);
+	return r;
 };
